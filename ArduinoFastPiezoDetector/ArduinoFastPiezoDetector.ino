944613b0d595d2439b9cadf7269839c9e5a1df01
Jason Copp <jason.copp@initlive.com>
2022-03-26 22:41:30 -0400

    wip

--- ArduinoFastPiezoDetector/ArduinoFastPiezoDetector.ino
+++ ArduinoFastPiezoDetector/ArduinoFastPiezoDetector.ino
@@ -3,28 +3,19 @@ const byte adcPin1 = 1;  // A1
 const byte adcPin2 = 2;  // A2
 const byte adcPin3 = 3;  // A3
 
-const int MAX_RESULTS = 1;
+const byte CHANNELS = 4;
+volatile int maxResults[CHANNELS];
 
 volatile int counter = 0;
 volatile int missedSamples = 0;
 volatile int skippedSamples = 0;
+const byte displayPin = 1;
 
-volatile int results [MAX_RESULTS];
-volatile int results1 [MAX_RESULTS];
-volatile int results2 [MAX_RESULTS];
-volatile int results3 [MAX_RESULTS];
-
-volatile int resultNumber;
-volatile int resultNumber1;
-volatile int resultNumber2;
-volatile int resultNumber3;
-
-volatile int currentPin = 1;
-const int displayPin = 1;
-int adcValue [4];
+volatile byte currentPin = 0;
+int adcValue[4];
 
 volatile boolean inLoop;
-
+//byte loopNum = 0;
  
 void setup ()
   {
@@ -45,7 +36,13 @@ void setup ()
   ADMUX = bit (REFS0) | (adcPin0 & 7);
   ADCSRB = bit (ADTS0) | bit (ADTS2);  // Timer/Counter1 Compare Match B
   ADCSRA |= bit (ADATE);   // turn on automatic triggering
+
+  for (int i=0;i<CHANNELS;i++) {
+    maxResults[i]=0;
+  }
 } 
+
+int discard;
 
 // ADC complete ISR
 ISR (ADC_vect) {
@@ -57,27 +54,47 @@ ISR (ADC_vect) {
 
     switch(currentPin) {
       case 0:
-        if (ADC > results [0])
-          results [0] = ADC;
-        ADMUX = bit (REFS0) | (adcPin1 & 7);
+        if (ADC > maxResults[0])
+          maxResults[0] = ADC;
+          ADMUX = bit (REFS0) | (adcPin1 & 7);
         currentPin = 1;
         break;
-      case 1:     
-        if (ADC > results1 [0])
-          results1 [0] = ADC;
-        ADMUX = bit (REFS0) | (adcPin2 & 7);
+      case 1:
+//        discard = ADC;
+//        ADMUX = bit (REFS0) | (adcPin1 & 7);
         currentPin = 2;
         break;
-      case 2:
-        if (ADC > results2 [0])
-          results2 [0] = ADC;
-        ADMUX = bit (REFS0) | (adcPin3 & 7);
+      case 2:     
+        if (ADC > maxResults[1])
+          maxResults[1] = ADC;
+        ADMUX = bit (REFS0) | (adcPin2 & 7);
         currentPin = 3;
         break;
-      case 3:
-        if (ADC > results3 [0])
-          results3 [0] = ADC;
+      case 3:     
+//        discard = ADC;
+//        ADMUX = bit (REFS0) | (adcPin2 & 7);
+        currentPin = 4;
+        break;
+      case 4:
+        if (ADC > maxResults[2])
+          maxResults[2] = ADC;
+        ADMUX = bit (REFS0) | (adcPin3 & 7);
+        currentPin = 5;
+        break;
+      case 5:
+//        discard = ADC;
+//        ADMUX = bit (REFS0) | (adcPin3 & 7);
+        currentPin = 6;
+        break;
+      case 6:
+        if (ADC > maxResults[3])
+          maxResults[3] = ADC;
         ADMUX = bit (REFS0) | (adcPin0 & 7);
+        currentPin = 7;
+        break;
+      case 7:
+//        discard = ADC;
+//        ADMUX = bit (REFS0) | (adcPin0 & 7);
         currentPin = 0;
         break;
     }
@@ -87,6 +104,7 @@ EMPTY_INTERRUPT (TIMER1_COMPB_vect);
 
 
 void loop () {
+//  Serial.print ("LOOP");
   
   inLoop = true;
 
@@ -102,44 +120,47 @@ void loop () {
 
 
 
-    adcValue[0] = results[0];
-    adcValue[1] = results1[0];
-    adcValue[2] = results2[0];
-    adcValue[3] = results3[0];
-    results[0] = results1[0] = results2[0] = results3[0] = 0;
+    adcValue[0] = maxResults[0];
+    adcValue[1] = maxResults[1];
+    adcValue[2] = maxResults[2];
+    adcValue[3] = maxResults[3];
+   
+    maxResults[0] = maxResults[1] = maxResults[2] = maxResults[3] = 0;
   inLoop = false;
-
+  
 #define MIN_THRESHOLD 150
 
-//#define SERIAL_PLOT_MODE
+#define SERIAL_PLOT_MODE
 
 #ifndef SERIAL_PLOT_MODE
   for (int i = 0; i<2; i++) {
   if (adcValue[i] > MIN_THRESHOLD)
     {
-      Serial.print ("Pin ");
+      Serial.print ("Pin");
       Serial.print (i);
-      Serial.print (": ");
-      Serial.println(adcValue[i]);
-      
+      Serial.print (":");
+      Serial.print(adcValue[i]);
+      Serial.print (",");
+      Serial.println();
     }
   }
+  
 
 #else
-  Serial.print ("MissedSamples:");
-  Serial.print (missedSamples);
+//  Serial.print ("MissedSamples:");
+//  Serial.print (missedSamples);
 //  Serial.print ("Counter:");
 //  Serial.print (counter);
   Serial.print (",Pin0:");
   Serial.print (adcValue[0]);
   Serial.print (",Pin1:");
   Serial.print (adcValue[1]);
-//  Serial.print (",Pin2:");
-//  Serial.print (adcValue[2]);
-//  Serial.print (",Pin3:");
-//  Serial.print (adcValue[3]);
-  Serial.print (",ForScaling:");
-  Serial.print ("1023");
+  Serial.print (",Pin2:");
+  Serial.print (adcValue[2]);
+  Serial.print (",Pin3:");
+  Serial.print (adcValue[3]);
+//  Serial.print (",ForScaling:");
+//  Serial.print ("1023");
   Serial.println ();
 
 #endif
@@ -150,7 +171,7 @@ void loop () {
 // A lower minimum increases latency.
 // Keep this delay low enough that the buffers will not overfill, ie, skippedSamples remains 0.
 */
-#define MINIMUM_FREQUENCY 190
-  delay(10);
+#define MINIMUM_FREQUENCY 59
+  delay(590/MINIMUM_FREQUENCY);
   
 }

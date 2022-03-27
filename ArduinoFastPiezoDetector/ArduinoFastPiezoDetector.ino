64697f2a0785e962c9f242397084cc4a68168915
Jason Copp <jason.copp@initlive.com>
2022-03-27 00:15:00 -0400

    Clean, fast.

--- ArduinoFastPiezoDetector/ArduinoFastPiezoDetector.ino
+++ ArduinoFastPiezoDetector/ArduinoFastPiezoDetector.ino
@@ -1,21 +1,21 @@
-const byte adcPin0 = 0;  // A0
-const byte adcPin1 = 1;  // A1
-const byte adcPin2 = 2;  // A2
-const byte adcPin3 = 3;  // A3
+#define SERIAL_PLOT_MODE
+#define MIN_THRESHOLD 150
+#define ANALOGPINS 6
 
-const byte CHANNELS = 4;
+byte adcPin[ANALOGPINS];
+
+const byte CHANNELS = 6;
 volatile int maxResults[CHANNELS];
 
 volatile int counter = 0;
 volatile int missedSamples = 0;
-volatile int skippedSamples = 0;
-const byte displayPin = 1;
 
-volatile byte currentPin = 0;
-int adcValue[4];
+byte currentPin = 0;
+int adcValue[CHANNELS];
 
 volatile boolean inLoop;
-//byte loopNum = 0;
+
+EMPTY_INTERRUPT (TIMER1_COMPB_vect);
  
 void setup ()
   {
@@ -33,107 +33,76 @@ void setup ()
 
   ADCSRA =  bit (ADEN) | bit (ADIE) | bit (ADIF);   // turn ADC on, want interrupt on completion
   ADCSRA |= bit (ADPS2);  // Prescaler of 16
-  ADMUX = bit (REFS0) | (adcPin0 & 7);
+  ADMUX = bit (REFS0) | (adcPin[0] & 7);
   ADCSRB = bit (ADTS0) | bit (ADTS2);  // Timer/Counter1 Compare Match B
   ADCSRA |= bit (ADATE);   // turn on automatic triggering
 
   for (int i=0;i<CHANNELS;i++) {
     maxResults[i]=0;
   }
+
+  // This relies on A0 being 0, etc, which on the UNO is true. Mapped manually for clarity.
+  adcPin[0]=0;  
+  adcPin[1]=1;
+  adcPin[2]=2;
+  adcPin[3]=3;
+  adcPin[4]=4;
+  adcPin[5]=5;
 } 
 
-int discard;
+const byte DISCARD = 1;
+byte discardCounter = 0;
 
 // ADC complete ISR
 ISR (ADC_vect) {
-    counter++; 
     if (inLoop) {
-      missedSamples++;
+      #ifdef SERIAL_PLOT_MODE
+        missedSamples++;
+      #endif
+      return;
+    }
+    
+    if (discardCounter == DISCARD) {
+      currentPin++;
+      discardCounter = 0;
+    }
+    else {
+      discardCounter++;
       return;
     }
 
-    switch(currentPin) {
-      case 0:
-        if (ADC > maxResults[0])
-          maxResults[0] = ADC;
-          ADMUX = bit (REFS0) | (adcPin1 & 7);
-        currentPin = 1;
-        break;
-      case 1:
-//        discard = ADC;
-//        ADMUX = bit (REFS0) | (adcPin1 & 7);
-        currentPin = 2;
-        break;
-      case 2:     
-        if (ADC > maxResults[1])
-          maxResults[1] = ADC;
-        ADMUX = bit (REFS0) | (adcPin2 & 7);
-        currentPin = 3;
-        break;
-      case 3:     
-//        discard = ADC;
-//        ADMUX = bit (REFS0) | (adcPin2 & 7);
-        currentPin = 4;
-        break;
-      case 4:
-        if (ADC > maxResults[2])
-          maxResults[2] = ADC;
-        ADMUX = bit (REFS0) | (adcPin3 & 7);
-        currentPin = 5;
-        break;
-      case 5:
-//        discard = ADC;
-//        ADMUX = bit (REFS0) | (adcPin3 & 7);
-        currentPin = 6;
-        break;
-      case 6:
-        if (ADC > maxResults[3])
-          maxResults[3] = ADC;
-        ADMUX = bit (REFS0) | (adcPin0 & 7);
-        currentPin = 7;
-        break;
-      case 7:
-//        discard = ADC;
-//        ADMUX = bit (REFS0) | (adcPin0 & 7);
-        currentPin = 0;
-        break;
-    }
+    #ifdef SERIAL_PLOT_MODE
+      counter++; 
+    #endif
+    
+    if (currentPin == CHANNELS)
+      currentPin = 0;
+
+    if (ADC > maxResults[currentPin])
+      maxResults[currentPin] = ADC;
+    
+    byte pin = currentPin+1;
+    pin = pin%CHANNELS; // <-- yeah! if the pin is higher than CHANNELS, then "pin MOD CHANNELS" loops it back to 0. 
+    ADMUX = bit (REFS0) | (pin & 7);
+    
+    // Interestingly, if you combine the (currentPin+1)&CHANNELS on any line, there is often a failure incrementing in time. There must be a compiler optimization that fails.
 }
   
-EMPTY_INTERRUPT (TIMER1_COMPB_vect);
-
-
 void loop () {
-//  Serial.print ("LOOP");
-  
+
+  // the inLoop boolean prevents the ADC interrupt code from colliding with this loop while the work is done below. keep anything processor intensive OUT of this section of the loop, like Serial output or heavy math.
+  // on an UNO, this loop work occasionally causes ONE sample to be discarded, which is only 1/50,000 of a seconds' worth of data. That's great.
   inLoop = true;
 
-  // tests show this loop causes ~43 missedSamples. The number of samples skipped is less than the number sampled OUTSIDE this loop, so an alternating set of buffers would
-  // recover these otherwise lost samples, but we don't have enough memory to have a complete second set of buffers
-  //  original looping
-  // MissedSamples: 43 SkippedSamples: 0 Counter: 342 Samples: 92 Result on display pin: 361
-  // 
-  
-  // on-the-fly-averaging
-  // MissedSamples: 0  SkippedSamples: 0 Counter: 289 Samples: 92 Result on display pin: 3
-  // 12500 alternating samples per channel with 4 channels
+  for (int i=0;i<CHANNELS;i++) {
+    adcValue[i] = maxResults[i];
+    maxResults[i] = 0;
+  }
 
-
-
-    adcValue[0] = maxResults[0];
-    adcValue[1] = maxResults[1];
-    adcValue[2] = maxResults[2];
-    adcValue[3] = maxResults[3];
-   
-    maxResults[0] = maxResults[1] = maxResults[2] = maxResults[3] = 0;
   inLoop = false;
-  
-#define MIN_THRESHOLD 150
-
-#define SERIAL_PLOT_MODE
 
 #ifndef SERIAL_PLOT_MODE
-  for (int i = 0; i<2; i++) {
+  for (int i = 0; i<CHANNELS; i++) {
   if (adcValue[i] > MIN_THRESHOLD)
     {
       Serial.print ("Pin");
@@ -144,13 +113,15 @@ void loop () {
       Serial.println();
     }
   }
-  
 
 #else
-//  Serial.print ("MissedSamples:");
+// Min / Max for scaling the graph
+  Serial.print ("0");
+  Serial.print (",80");
+//  Serial.print (",MissedSamples:");
 //  Serial.print (missedSamples);
-//  Serial.print ("Counter:");
-//  Serial.print (counter);
+  Serial.print (",Counter:"); 
+  Serial.print (counter);
   Serial.print (",Pin0:");
   Serial.print (adcValue[0]);
   Serial.print (",Pin1:");
@@ -159,19 +130,25 @@ void loop () {
   Serial.print (adcValue[2]);
   Serial.print (",Pin3:");
   Serial.print (adcValue[3]);
-//  Serial.print (",ForScaling:");
-//  Serial.print ("1023");
   Serial.println ();
-
-#endif
   counter = missedSamples = 0;
+#endif
+
   
-/* This delay is very specific to the frequency of the pieze sensor being monitored.
-// Frequencies below this minimum are not guarenteed to register.
-// A lower minimum increases latency.
-// Keep this delay low enough that the buffers will not overfill, ie, skippedSamples remains 0.
-*/
-#define MINIMUM_FREQUENCY 59
-  delay(590/MINIMUM_FREQUENCY);
+/*  This delay ensures enough time to prevent aliasing. 
+ *  For example:
+ *  If counter == 50000 per second, and CHANNELS == 6, then you'll get 8333 samples per second.
+ *  Half are discarded by the DISCARD system to allow the analog pin to settle.
+ *  Half again can be considered discarded by being a negative voltage (unless you either full bridge recitfy (BOOOM) or bias the ac signal, which you should.
+ *  In my test, my UNO with 6 channels, i could sample down to 80Hz without missing the peak with a delay of 10, thus getting a stable max value on a sine wave with an acceptable latency.
+ *  Less channels and higher minimum freq means you can lower this delay.
+ *  
+ *  Note that at HIGHER piezo freqencies, the sampling algo will break down if they match the sample rate or are multiple of it, due to the sample peak being 'skipped over'.
+ *  This can be addressed by sampling one channel at a much higher freq for CHANNEL # of cycles ones, but that can cause slightly less accuracy to get missed at lower freqs occasionally too.
+ *  It's more logic in the interrupt though, which is bad.
+ *  
+ *  The short of it is: tune the system to your expected piezo frequency. This will NOT do higher AND lower freqs perfectly without compromise.
+ */
   
+  delay(10);
 }
